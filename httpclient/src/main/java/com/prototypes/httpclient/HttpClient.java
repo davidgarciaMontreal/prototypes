4436aa332b11fd107404ec6ff6bcd4d9b4783569
David Garcia-Campos <dgarciacampos@gmail.com>
2017-12-09 13:48:00 -0500

    refactor http handling

--- httpclient/src/main/java/com/prototypes/httpclient/HttpClient.java
+++ httpclient/src/main/java/com/prototypes/httpclient/HttpClient.java
@@ -1,46 +1,19 @@
 package com.prototypes.httpclient;
-import java.io.IOException;
-import java.util.Collections;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import java.util.Collections;
 
 public class HttpClient {
     public static int SUC_CODE = 0;
+
     public final static void main(String[] args) throws Exception {
+
         System.out.println("Init http client");
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        try {
-            HttpGet httpget = new HttpGet("https://www.google.com");
-            System.out.println("Executing request " + httpget.getRequestLine());
-            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
 
-                @Override
-                public String handleResponse(
-                        final HttpResponse response) throws ClientProtocolException, IOException {
-                    int status = response.getStatusLine().getStatusCode();
-                    if (status >= 200 && status < 300) {
-                        HttpEntity entity = response.getEntity();
-                        return entity != null ? EntityUtils.toString(entity) : null;
-                    } else {
-                        throw new ClientProtocolException("Unexpected response status: " + status);
-                    }
-                }
-
-            };
-            String responseBody = httpclient.execute(httpget, responseHandler);
-            String header_stdout = String.join("", Collections.nCopies(100, "#"));
-            System.out.println(header_stdout);
-            System.out.println(responseBody);
-        } finally {
-            httpclient.close();
-        }
+        MyHttpClient cl = new MyHttpClient("https://www.google.com");
+        String responseBody = cl.getBody();
+        String header_stdout = String.join("", Collections.nCopies(100, "#"));
+        System.out.println(header_stdout);
+        System.out.println(responseBody);
         System.exit(SUC_CODE);
     }
 }
---
+++ httpclient/src/main/java/com/prototypes/httpclient/MyHttpClient.java
@@ -0,0 +1,61 @@
+package com.prototypes.httpclient;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
+
+public class MyHttpClient {
+
+    private String url;
+
+    public MyHttpClient(String url) {
+
+        try {
+            new URL(url);
+        } catch ( MalformedURLException mal) {
+            mal.printStackTrace();
+        }
+
+        this.url = url;
+    }
+
+    public String getBody() throws IOException {
+
+        String responseBody = "";
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        try {
+            HttpGet httpget = new HttpGet(this.url);
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+                @Override
+                public String handleResponse(
+                        final HttpResponse response) throws ClientProtocolException, IOException {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+
+            };
+            responseBody = httpclient.execute(httpget, responseHandler);
+        } finally {
+            httpclient.close();
+        }
+        return responseBody;
+    }
+}
