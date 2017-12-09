0fd29b2325d6ff522ec8cd286a7ba77a61414970
David Garcia-Campos <dgarciacampos@gmail.com>
2017-12-09 11:50:43 -0500

    add main class

---
+++ httpclient/src/main/java/com/prototypes/httpclient/HttpClient.java
@@ -0,0 +1,9 @@
+package com.prototypes.httpclient;
+
+public class HttpClient {
+    public static int SUC_CODE = 0;
+    public static void main(String[] args){
+        System.out.println("Init http client");
+        System.exit(SUC_CODE);
+    }
+}
